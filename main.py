51674987e04191300af7a8a24c8bb48c9d91a815
tatianadeutsch <106558186+tatianadeutsch@users.noreply.github.com>
2022-11-29 18:21:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from keyboard import sender
+from main import *
+
+for event in bot.longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+        request = event.text.lower()
+        user_id = str(event.user_id)
+        msg = event.text.lower()
+        sender(user_id, msg.lower())
+        if request == 'новый поиск':
+            creating_database()
+            bot.write_msg(user_id, f'Привет, {bot.name(user_id)}')
+            bot.find_user(user_id)
+            bot.write_msg(event.user_id, f'Ваш визави найден! Выберите "Следующая анкета"')
+            bot.find_persons(user_id, offset)
+
+        elif request == 'следующая анкета':
+            for i in line:
+                offset += 1
+                bot.find_persons(user_id, offset)
+                break
+
+        else:
+            bot.write_msg(event.user_id, 'Ваше сообщение непонятно')
